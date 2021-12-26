aeaa5c92d0e8d50af35d72923334b650bc78eb8c
Tyquan <tyquanjreddick@gmail.com>
2021-12-26 13:56:15 -0500

    updated compiler

--- bdfp/ch3/moolahcoin/contracts/Condos.sol
+++ bdfp/ch3/moolahcoin/contracts/Condos.sol
@@ -1,19 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Condos is ERC721, ERC721URIStorage {
+contract Condos is ERC721 {
     // Setup like a game Nft
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    //string name = ;
+    //string symbol = ;
+
     mapping(address => uint256) public MDTrack;
 
-    constructor() ERC721("CondosItem", "CND") {}
-
+    constructor() ERC721("CondosItem", "CND") {
+        //_burn(_tokenIds);
+    }
+    /*
     function createNFT(address receiver, string memory tokenURI) public returns (uint256)
     {
         _tokenIds.increment();
@@ -27,14 +32,19 @@ contract Condos is ERC721, ERC721URIStorage {
         return newItemId;
     }
 
-    function transferNFT(address sender, address receiver, uint256 transId, string memory tokenURI) public
+    
+    function transferNFT(address sender, address receiver, uint256 transId, string memory tokenURI) external payable returns (bool)
     {
-        _transferFrom(sender, receiver, transId);
+        transferFrom(sender, receiver, transId);
         _setTokenURI(transId, tokenURI);
 
         delete MDTrack[sender];
 
         MDTrack[receiver] = _tokenIds.current();
+
+        return true;
     }
+
+    */
 
 }
--- bdfp/ch3/moolahcoin/contracts/MoolahCoin.sol
+++ bdfp/ch3/moolahcoin/contracts/MoolahCoin.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MoolahCoin is ERC20 {
-    constructor() public ERC20("Moolah Coin", "MC") {
+    constructor() ERC20("Moolah Coin", "MC") {
         _mint(msg.sender, 100000 * (10 ** uint256(decimals())));
     }
 }
